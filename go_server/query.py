041189af5aa9160296db9f17854b153f3f51e3fd
ShuWei Kuo <shuwei.kuo.tw@gmail.com>
2021-01-31 14:52:35 +0800

    server

---
+++ go_server/query.py
@@ -0,0 +1,10 @@
+import httplib2
+
+h = httplib2.Http(".cache")
+h.add_credentials('user', 'pass')
+r, content = h.request("https://journey-of-foobar-server.herokuapp.com",
+  "PUT", body="This is text",
+  headers={'content-type':'text/plain'} )
+h.close()
+print r['status']
+print r['content-type']
