d40c5ceabf48ed0e4d3bc0a1de8f3d894ef1e881
Josh Kuo <Josh.Kuo@dlinkcorp.com>
2021-03-24 17:59:00 +0800

    rotate

--- solution/4-2.py
+++ solution/4-2.py
@@ -4,7 +4,7 @@ bcolors = colourtheme.bcolors()
 class vectors:
   def __init__(self, dimensions, your_position, trainer_position, distance):
     self.dimensions = dimensions
-    # self.g = [['_' for column in range(dimensions[0]+1)] for row in range(dimensions[1]+1)]
+    self.g = [['_' for column in range(dimensions[0]+1)] for row in range(dimensions[1]+1)]
     self.y = your_position
     self.t = trainer_position
     self.distance = distance
@@ -25,19 +25,15 @@ class vectors:
             dimension = self.dimensions[index] + 1
             temp.append(self.bounce(v, dimension))
           self.possibilities.add((i, j))
-        if 9< len(self.possibilities) < 11:
-          break
           # print 'temp',bcolors.WARNING,delta_vector,'->',temp,bcolors.ENDC,'\n'
-          # if temp[0] == self.diffwidth and temp[1] == self.diffheight:
-          #   print 'same vector',bcolors.WARNING,delta_vector,'->',temp,bcolors.ENDC,'\n'
-          #   if math.hypot(delta_vector[0], delta_vector[1]) <= self.distance:
-          #     # if list(delta_vector) not in self.l:
-          #       # print 'distance is ',math.hypot(delta_vector[0], delta_vector[1]),'which is within',self.distance
-          #     print '',bcolors.WARNING,delta_vector,'->',temp,bcolors.ENDC,'\n'
+          if temp[0] == self.diffwidth and temp[1] == self.diffheight:
+            # print 'same vector',bcolors.WARNING,delta_vector,'->',temp,bcolors.ENDC,'\n'
+            if math.hypot(delta_vector[0], delta_vector[1]) <= self.distance:
+              # if list(delta_vector) not in self.l:
+                # print 'distance is ',math.hypot(delta_vector[0], delta_vector[1]),'which is within',self.distance
+              print '',bcolors.WARNING,delta_vector,'->',temp,bcolors.ENDC,'\n'
     print 'possibilities',len(self.possibilities)
     # for delta_vector in self.possibilities:
-
-
 
   def bounce(self, v, dimension):
     if v >= dimension:
@@ -45,12 +41,15 @@ class vectors:
     else:
       return v % 2
 
+def distances():
+
+
 def solution(dimensions, your_position, trainer_position, distance):
   vector = vectors(dimensions, your_position, trainer_position, distance)
-  dic = vector.__dict__
-  print 'dic',dic
+  # dic = vector.__dict__
+  # print 'dic',dic
   # print(json.dumps(dic, indent = 4))
   vector.possible()
 
-# solution([3,2], [1,1], [2,1], 4)
-solution([300,275], [150,150], [185,100], 500)
+solution([3,2], [1,1], [2,1], 4)
+# solution([300,275], [150,150], [185,100], 500)
