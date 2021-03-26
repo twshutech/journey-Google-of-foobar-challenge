9a8d2d78ea41a73a4108a361b5aa50317e20bdef
ShuWei Kuo <shuwei.kuo.tw@gmail.com>
2021-03-26 08:36:11 +0800

    filter

--- solution/4-2.py
+++ solution/4-2.py
@@ -2,11 +2,15 @@ import colourtheme, math, json
 import numpy as np
 bcolors = colourtheme.bcolors()
 # def distances():
+dis = lambda i,j,ti,tj:math.hypot(ti-i, tj-j)
+field = lambda i,j,ti,tj,m: np.inf if dis(i,j,ti,tj) > m else dis(i,j,ti,tj)
+highlight = lambda n: int(n) if n != np.inf else np.inf
 
 class vectors:
   def __init__(self, dimensions, your_position, trainer_position, distance):
     self.dimensions = dimensions
-    self.g = np.array([[column+row for column in range(dimensions[0]+1)] for row in range(dimensions[1]+1)])
+    self.test = np.array([[round(field(i,j,trainer_position[1],trainer_position[0],distance)) for j in range(-1*distance,distance+1,1)] for i in range(-1*distance,distance+1,1)])
+    self.g = np.array([[field(column,row,trainer_position[1],trainer_position[0],distance) for column in range(dimensions[0]+1)] for row in range(dimensions[1]+1)])
     self.y = np.array(your_position)
     self.t = np.array(trainer_position)
     self.distance = distance
@@ -18,7 +22,7 @@ class vectors:
     self.diffheight = abs(trainer_position[1]-your_position[1])
     #vector
     self.diff = np.array([trainer_position[0]-your_position[0], trainer_position[1]-your_position[1]])
-    self.l = [[1, 0], [1, 2], [1, -2], [3, 2], [3, -2], [-3, 2], [-3, -2]]
+    self.l = np.array([[1, 0], [1, 2], [1, -2], [3, 2], [3, -2], [-3, 2], [-3, -2]])
 
   def rot(self, to):
     if to == 0:
@@ -26,7 +30,7 @@ class vectors:
     else:
       self.g = np.flipud(self.g)
     print np.asmatrix(self.g)
-
+# [[j+i for j in range(-1*distance,distance+1,1)] for i in range(-1*distance,distance+1,1)]
   def possible(self):
     for i in range(-1*self.distance,self.distance+1,1):
       for j in range(-1*self.distance,self.distance+1,1):
@@ -40,15 +44,17 @@ class vectors:
             if math.hypot(delta_vector[0], delta_vector[1]) <= self.distance:
               # if list(delta_vector) not in self.l:
                 # print 'distance is ',math.hypot(delta_vector[0], delta_vector[1]),'which is within',self.distance
-              print '',bcolors.WARNING,delta_vector,'->',temp,bcolors.ENDC
-              s = self.y+np.array(delta_vector)
-              angle = np.arctan2(s[0],s[1])
+              # print '',bcolors.WARNING,delta_vector,'->',temp,bcolors.ENDC
+              # s = np.array(delta_vector)+ self.t
+              # angle = np.arctan2(s[0],s[1])
+              angle = np.arctan2(i,j)
               if angle not in self.angles:
                 self.possibilities.add((i, j))
               self.angles.add(angle)
-              print self.y+np.array(delta_vector),'\n'
-              print len(self.possibilities),self.possibilities
-              print len(self.angles),self.angles
+              # print self.y+np.array(delta_vector),'\n'
+    print bcolors.WARNING,len(self.possibilities),self.possibilities,bcolors.ENDC
+    print
+    print bcolors.OKGREEN,len(self.angles),self.angles,bcolors.ENDC
               # self.angles.add((self.y+np.array(delta_vector)))
               # self.__init__(self.dimensions, self.y, self.t, self.distance)
     # print self.angles
@@ -68,6 +74,10 @@ def solution(dimensions, your_position, trainer_position, distance):
   # print 'dic',dic
   # print(json.dumps(dic, indent = 4))
   vector.possible()
+  # for dot in vector.possibilities:
+  #   print dot,'->',dot[0] * dot[1]
+  #   vector.test[dot[1]][dot[0]] = dot[0] * dot[1]
+  print vector.test
 
 solution([3,2], [1,1], [2,1], 4)
 # solution([300,275], [150,150], [185,100], 500)
