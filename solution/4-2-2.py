266ebfb496caffa71ecab487a0e34b275d70dc7b
Josh Kuo <Josh.Kuo@dlinkcorp.com>
2021-03-31 12:21:26 +0800

    tired

--- solution/4-2-2.py
+++ solution/4-2-2.py
@@ -1,11 +1,16 @@
 import numpy as np
+import math
 
 unit_vector = lambda vector:vector / np.linalg.norm(vector)
-vector_magnitude = lambda m: np.hypot(m[0], m[1])
+vector_magnitude = lambda m: math.hypot(m[0], m[1])
 vector_angle = lambda m: np.arctan2(m[0], m[1]) * 180 / np.pi
-scalar = lambda
-isntfar = lambda m, max_distance: True if vector_magnitude(m) <= max_distance else False
-collision = lambda
+# scalar = lambda
+isntfar = lambda m, scalar: True if vector_magnitude(m) <= scalar else False
+# collision = lambda
+rounds_bouncing = lambda s, r: s//r
+# return the least one.
+closest = lambda s1, s2: np.less([s1, s2])
+
 class vectors:
   def __init__(self, dimensions, your_position, trainer_position, distance):
     self.dimensions = dimensions
@@ -16,6 +21,7 @@ class vectors:
     self.directions = set([])
     self.d = np.array([(1,1), (-1,1), (1, -1), (-1, -1)])
     self.towall = np.array([])
+    self.angle_dict = ({})
 
   def direction(self):
     self.getrelation(self.your_position)
@@ -29,22 +35,50 @@ class vectors:
         # print isntfar(dot, self.distance)
         if isntfar(dot, self.distance):
           angle = vector_angle(dot)
+          # self.update_angle(angle, dot)
           if angle not in angles:
-            tempdot = []
             angles.add(angle)
-            print 'dot',dot,self.towall
-            tempdot[0] = collision(dot[0], )
+            # print ('dot', dot,self.towall)
+            # self.bounce(dot, )
+            # self.bounce()
           # print angles, len(angles),'\n'
           #   print dot[0],dot[1], dot
           # temp.add(list(dot))
-    print len(angles)
+    print( len(angles))
 
   def getrelation(self, dot):
-    self.towall = np.array([-1 * dot[1], self.dimensions[1]-dot[1], -1 * dot[0], self.dimensions[0]-dot[0]])
+    self.towall = np.array([-1 * dot[1], self.dimensions[1]-dot[1], -1 * dot[0], self.dimensions[0]-dot[0]], dtype=float)
 
-  def bounce(self):
+  def update_angle(self, angle, dot):
+    # print( 'close',closest(, ))
+    if angle in self.angle_dict.keys():
+      # scalar = self.angle_dict[angle]
+      self.angle_dict.update({angle: dot})
+
+      print(dot,vector_magnitude(dot), vector_magnitude(self.angle_dict[angle]))
+
+  def quadrant(self, angle):
+    if 90 > angle >= 0:
+      return  np.take(self.towall, [1,4])
+    elif 180 > angle >= 90:
+      return  np.take(self.towall, [0,4])
+    elif 270 > angle >= 180:
+      return  np.take(self.towall, [1,3])
+    elif 360 > angle >= 270:
+      return  np.take(self.towall, [0,3])
+
+  def vector_destination(self, v, angle):
+    sx = abs(v[0])
+    sy = abs(v[1])
+    direction = self.quadrant(angle)
 
 
+  def bounce(self, s, d):
+    c = 0
+    while (s % d)>=d:
+      s /= d
+      c += 1
+    print('s',s,'c',c)
 
 def solution(dimensions, your_position, trainer_position, distance):
   v = vectors(dimensions, your_position, trainer_position, distance)
